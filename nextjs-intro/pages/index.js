f1c22c7742df6282ddc6bc7145400a6240a485e8
David C <wooritobi@gmail.com>
2022-06-26 15:14:29 +0900

    1.3 Routing (NavBar component)

--- nextjs-intro/pages/index.js
+++ nextjs-intro/pages/index.js
@@ -1,11 +1,10 @@
 import {useState} from "react";
-
+import NavBar from "../components/NavBar";
 export default function Home() {
-    const [counter, setCounter] = useState(0)
     return (
         <div>
-            <h4>Counter: {counter}</h4>
-            <button onClick = {()=>{setCounter((prev)=>prev+1)}}>+</button>
+            <NavBar />
+            <h4>Home</h4>
         </div>
     )
 }
